7695084b5e0307c580fad00c64e65d7f84d1c1e4
isaac <yitzaklr@gmail.com>
2017-02-28 16:37:40 -0500

    ???

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/process_get', function (req, res) {
       },
       function (error, response, body) {
          console.log("parsing");
-         JSON.parse(body);
+         var object = JSON.parse(body);
       });
 })
 
